da822656c7387bff1ae744b91061a10b4193d947
derek <gaffneyd4@students.rowan.edu>
2017-10-04 16:06:11 -0400

    Somehow forgot this file

--- py/example-app.py
+++ py/example-app.py
@@ -1,23 +1,30 @@
 
 from flask import Flask
 from flask import jsonify
+from flask_mysqldb import MySQL
 
 app = Flask (__name__)
 
-posts = [
-    {
-        'id': 1,
-        'title': u'Section 101 Row 10 Seat 12',
-        'description': u'Great seats',
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Nosebleeds',
-        'description': u'Terrible seats',
-        'done': False
-    }
-]
+mysql = MySQL(app)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'ttt'
+
+# posts = [
+#     {
+#         'id': 1,
+#         'title': u'Section 101 Row 10 Seat 12',
+#         'description': u'Great seats',
+#         'done': False
+#     },
+#     {
+#         'id': 2,
+#         'title': u'Nosebleeds',
+#         'description': u'Terrible seats',
+#         'done': False
+#     }
+# ]
 
 @app.after_request
 def after_request(response):
@@ -28,6 +35,10 @@ def after_request(response):
 
 @app.route('/users')
 def index():
+    conn = mysql.connection
+    cursor = conn.cursor()
+    cursor.execute("SELECT accountID, email, password, timestamp FROM accounts")
+    posts = [dict(accountID=row[0], email=row[1], password=row[2], timestamp=row[3]) for row in cursor.fetchall()]
     return jsonify({'posts': posts})
 
 if __name__ == '__main__':
