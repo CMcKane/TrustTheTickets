61c75094618da60d2769e4bf5f23ce8933072724
derek <gaffneyd4@students.rowan.edu>
2017-10-12 14:10:46 -0400

    Finished TTT_V2 Model - should be consistent data model. Loaded my db with data and did a data export into the dump folder TTT_V2_Dump.

---
+++ schema/TTT_V2_Dump/ttt_accounts.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: ttt
+-- ------------------------------------------------------
+-- Server version	5.7.17-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `accounts`
+--
+
+LOCK TABLES `accounts` WRITE;
+/*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
+INSERT INTO `accounts` VALUES (1,'gaffneyd4@students.rowan.edu','password',1,'2017-10-12 12:37:51','Derek','Gaffney','100 Redmond Ave. Rowan Blvd. Apt.#3425','Glassboro',30,'08028',1,'8568349018',NULL,1,NULL),(2,'harkert2@students.rowan.edu','password',1,'2017-10-12 12:38:06','Tom','Harker','201 Mullica Hill Road','Glassboro',30,'08028',1,'8562564000',NULL,1,NULL),(3,'orioa9@students.rowan.edu','password',1,'2017-10-12 12:38:11','Anthony','Orio','201 Mullica Hill Road','Glassboro',30,'08028',1,'8562564000',NULL,1,NULL),(4,'bailliec7@students.rowan.edu','password',1,'2017-10-12 12:38:15','Curtis','Baillie','201 Mullica Hill Road','Glassboro',30,'08028',1,'8562564000',NULL,1,NULL),(5,'dalonzoj8@students.rowan.edu','password',1,'2017-10-12 12:38:16','Jon','D\'Alonzo','201 Mullica Hill Road','Glassboro',30,'08028',1,'8562564000',NULL,1,NULL),(6,'mckanec8@students.rowan.edu','password',1,'2017-10-12 12:38:20','Chris','McKane','201 Mullica Hill Road','Glassboro',30,'08028',1,'8562564000',NULL,1,NULL);
+/*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-10-12 14:07:25
---
+++ schema/TTT_V2_Dump/ttt_state_prov.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: ttt
+-- ------------------------------------------------------
+-- Server version	5.7.17-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `state_prov`
+--
+
+LOCK TABLES `state_prov` WRITE;
+/*!40000 ALTER TABLE `state_prov` DISABLE KEYS */;
+INSERT INTO `state_prov` VALUES (1,'AL','Alabama'),(2,'AK','Alaska'),(3,'AZ','Arizona'),(4,'AR','Arkansas'),(5,'CA','California'),(6,'CO','Colorado'),(7,'CT','Connecticut'),(8,'DE','Delaware'),(9,'FL','Florida'),(10,'GA','Georgia'),(11,'HI','Hawaii'),(12,'ID','Idaho'),(13,'IL','Illinois'),(14,'IN','Indiana'),(15,'IA','Iowa'),(16,'KS','Kansas'),(17,'KY','Kentucky'),(18,'LA','Louisiana'),(19,'ME','Maine'),(20,'MD','Maryland'),(21,'MA','Massachusetts'),(22,'MI','Michigan'),(23,'MN','Minnesota'),(24,'MS','Mississippi'),(25,'MO','Missouri'),(26,'MT','Montana'),(27,'NE','Nebraska'),(28,'NV','Nevada'),(29,'NH','New Hampshire'),(30,'NJ','New Jersey'),(31,'NM','New Mexico'),(32,'NY','New York'),(33,'NC','North Carolina'),(34,'ND','North Dakota'),(35,'OH','Ohio'),(36,'OK','Oklahoma'),(37,'OR','Oregon'),(38,'PA','Pennsylvania'),(39,'RI','Rhode Island'),(40,'SC','South Carolina'),(41,'SD','South Dakota'),(42,'TN','Tennessee'),(43,'TX','Texas'),(44,'UT','Utah'),(45,'VT','Vermont'),(46,'VA','Virginia'),(47,'WA','Washington'),(48,'WV','West Virginia'),(49,'WI','Wisconsin'),(50,'WY','Wyoming'),(51,'AS','American Samoa'),(52,'DC','District of Columbia'),(53,'FM','Federated States of Micronesia'),(54,'GU','Guam'),(55,'MH','Marshall Islands'),(56,'MP','Northern Mariana Islands'),(57,'PW','Palau'),(58,'PR','Puerto Rico'),(59,'VI','Virgin Islands'),(60,'AB','Alberta'),(61,'BC','British Columbia'),(62,'MB','Manitoba'),(63,'NB','New Brunswick'),(64,'NL','Newfoundland and Labrador'),(65,'NS','Nova Scotia'),(66,'NT','Northwest Territories'),(67,'NU','Nunavut'),(68,'ON','Ontario'),(69,'PE','Prince Edward Island'),(70,'QC','Quebec'),(71,'SK','Saskatchewan'),(72,'YT','Yukon');
+/*!40000 ALTER TABLE `state_prov` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-10-12 14:07:27
