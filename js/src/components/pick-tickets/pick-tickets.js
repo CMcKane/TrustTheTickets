f6e6d2c4bcc28ae8df9561097400aa90578691b9
AnthonyOrio <orioa94@gmail.com>
2017-10-28 15:17:22 -0400

    Added slider and field

--- js/src/components/pick-tickets/pick-tickets.js
+++ js/src/components/pick-tickets/pick-tickets.js
@@ -5,6 +5,7 @@ import '../pick-tickets/pick-tickets.css';
 import _ from 'lodash';
 import WellsFargoChart from './wells-fargo-chart';
 import { TTTPost } from '../backend/ttt-request';
+import ReactSliderNativeBootstrap from 'react-bootstrap-native-slider';
 
 export default class PickTickets extends Component {
 
@@ -13,7 +14,8 @@ export default class PickTickets extends Component {
 
         this.state = {
             section: '',
-            tickets: []
+            tickets: [],
+            price: 0
         }
     }
 
@@ -56,6 +58,29 @@ export default class PickTickets extends Component {
                     </Col>
                     <Col lg={4}>
                         {<div>
+
+                            <ReactSliderNativeBootstrap
+                                     max={50}
+                                     min={1}
+                                     step={1}
+                                     tooltip="hide"
+                                     change={this.firstComponentChangeValue}
+                                     value={this.state.firstComponentCurrentValue}
+                                />
+
+                                <ControlLabel
+                                    id = "sliderPrice"
+                                    style={{color: 'white', fontSize: 15}}>
+                                    {this.state.price}
+                                </ControlLabel>
+
+                            <form>
+                                <ControlLabel style={{color: 'white', fontSize: 15}}>Section #: </ControlLabel>
+                                <input name="param1" value="test"/>
+                                <input type="submit" name="" id="search-submit" class="button" value="Submit"/>
+                            </form>
+
+
                             <FormGroup controlId="formControlsSelectMultiple">
                                 <ControlLabel style={{color: 'white', fontSize: 25}}>Here are your ticket options for selected section: </ControlLabel>
                                 <FormControl style={{height: '650px'}} componentClass="select" multiple>
