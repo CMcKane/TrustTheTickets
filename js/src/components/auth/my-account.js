5a95d3ba1c95fdca3e4ab43eb75736ff12a188ba
Chris McKane <chrisrmckane@gmail.com>
2017-10-31 22:11:25 -0400

    Quick background changes for pages. Better theme and colors to come.

--- js/src/components/auth/my-account.js
+++ js/src/components/auth/my-account.js
@@ -1,22 +1,25 @@
-import React, { Component }  from 'react';
-import { Button } from 'react-bootstrap';
-import { Redirect } from 'react-router-dom';
+import React, {Component} from 'react';
+import {Button} from 'react-bootstrap';
+import {Redirect} from 'react-router-dom';
+import '../../App.css';
 
 export default class MyAccount extends Component {
 
     render() {
         if (this.props.user.loggedIn) {
             return (
-                <div className='text-center' style={{color: 'white'}}>
-                    <h1>My Account</h1>
-                    <h3>Welcome {this.props.user.fname} {this.props.user.lname}!</h3>
+                <div className='homeBody'>
+                    <div className='text-center' style={{color: 'white', paddingTop: '5%'}}>
+                        <h1>My Account</h1>
+                        <h3>Welcome {this.props.user.fname} {this.props.user.lname}!</h3>
                         <Button bsStyle='primary'
-                        onClick={this.props.logOut.bind(this)}>
-                        Log Out
-                    </Button>
+                                onClick={this.props.logOut.bind(this)}>
+                            Log Out
+                        </Button>
+                    </div>
                 </div>
             );
         }
-        return <Redirect to='/login' />
+        return <Redirect to='/login'/>
     }
 }
--- js/src/components/pick-tickets/pick-tickets.js
+++ js/src/components/pick-tickets/pick-tickets.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {Grid, Row, Col, FormGroup, ControlLabel, FormControl, Well, Button, Panel} from 'react-bootstrap';
 import '../../seating-chart.css';
 import '../pick-tickets/pick-tickets.css';
 import _ from 'lodash';
 import WellsFargoChart from './wells-fargo-chart';
-import { TTTPost } from '../backend/ttt-request';
+import {TTTPost} from '../backend/ttt-request';
 import ReactSliderNativeBootstrap from 'react-bootstrap-native-slider';
 import TicketListItem from './ticket-list-item';
 import queryString from 'query-string';
@@ -24,7 +24,7 @@ export default class PickTickets extends Component {
     }
 
     handleChange(e) {
-        this.setState({ [e.target.name]: e.target.value });
+        this.setState({[e.target.name]: e.target.value});
     }
 
     getTicketsWithFilter() {
@@ -32,22 +32,22 @@ export default class PickTickets extends Component {
             price: this.state.price,
             section: this.state.section
         })
-        .then(res => {
-            if (res.data.tickets) this.setState({
-                tickets: res.data.tickets
+            .then(res => {
+                if (res.data.tickets) this.setState({
+                    tickets: res.data.tickets
+                });
             });
-        });
     }
 
     getAllTickets(eventID) {
         TTTPost('/all-ticket', {
             eventID: this.state.eventID
         })
-        .then(res => {
-            if (res.data.tickets) this.setState({
-                tickets: res.data.tickets
+            .then(res => {
+                if (res.data.tickets) this.setState({
+                    tickets: res.data.tickets
+                });
             });
-        });
     }
 
     getSelectedGame() {
@@ -61,31 +61,31 @@ export default class PickTickets extends Component {
 
     _onButtonClick() {
         this.setState({
-          showFilter: true
+            showFilter: true
         });
     }
 
 
     onChartClick(section) {
-        if(section.length > 0) {
+        if (section.length > 0) {
             TTTPost('/tickets', {
                 section_number: section
             })
-            .then(res => {
-                if (res.data.tickets) this.setState({
-                    section: section,
-                    tickets: res.data.tickets
+                .then(res => {
+                    if (res.data.tickets) this.setState({
+                        section: section,
+                        tickets: res.data.tickets
+                    });
                 });
-            });
         }
     }
 
     firstComponentChangeValue(e) {
         console.log(e.target.value);
-      this.setState({ 
-        firstComponentCurrentValue: e.target.value,
-        price: e.target.value
-      });
+        this.setState({
+            firstComponentCurrentValue: e.target.value,
+            price: e.target.value
+        });
     }
 
     renderTickets() {
@@ -101,74 +101,78 @@ export default class PickTickets extends Component {
         const eventID = queryParams.event;
         this.getAllTickets(eventID);
         return (
-            <Grid style={{paddingTop: "100px"}}>
-                <h1 className="border-white">
-                    <Well className='pick-tickets-well' style={{background: 'transparent'}}>
-                        Choose Your Desired Section From The Seating Chart
-                    </Well>
-                </h1>
-                <Row>
-                    <Col lg={8}>
-                        <Col lg={4}>
-                            <Button onclick={this.getSelectedGame()}>
-                                {eventID}
-                            </Button>
+            <div>
+                <Grid style={{paddingTop: "100px"}}>
+                    <h1 className="border-white">
+                        <Well className='pick-tickets-well' style={{background: 'transparent'}}>
+                            Choose Your Desired Section From The Seating Chart
+                        </Well>
+                    </h1>
+                    <Row>
+                        <Col lg={8}>
+                            <Col lg={4}>
+                                <Button onclick={this.getSelectedGame()}>
+                                    {eventID}
+                                </Button>
+                            </Col>
+                            <WellsFargoChart
+                                onSectionSelected={this.onChartClick.bind(this)}
+                                selectedSection={this.state.section}/>
                         </Col>
-                    <WellsFargoChart
-                    onSectionSelected={this.onChartClick.bind(this)}
-                    selectedSection={this.state.section}/>
-                    </Col>
-                    <Col lg={4}>
+                        <Col lg={4}>
 
-                        <Button onClick={() => this.setState({ showFilter: !this.state.showFilter })}>
-                          Filter
-                        </Button>
-                        <Panel collapsible expanded={this.state.showFilter}>
-                            <FormGroup controlId="formControlsEmail">
-                                <ReactSliderNativeBootstrap
-                                    className="price-slider"
-                                    max={1000}
-                                    min={1}
-                                    step={1}
-                                    tooltip="hide"
-                                    handleChange={this.firstComponentChangeValue.bind(this)}
-                                    value={this.state.firstComponentCurrentValue}
-                                />
+                            <Button onClick={() => this.setState({showFilter: !this.state.showFilter})}>
+                                Filter
+                            </Button>
+                            <Panel collapsible expanded={this.state.showFilter}>
+                                <FormGroup controlId="formControlsEmail">
+                                    <ReactSliderNativeBootstrap
+                                        className="price-slider"
+                                        max={1000}
+                                        min={1}
+                                        step={1}
+                                        tooltip="hide"
+                                        handleChange={this.firstComponentChangeValue.bind(this)}
+                                        value={this.state.firstComponentCurrentValue}
+                                    />
 
-                                <ControlLabel
-                                   className="slider-price-label">
-                                   Ticket Price: ${this.state.price}
-                                </ControlLabel>
+                                    <ControlLabel
+                                        className="slider-price-label">
+                                        Ticket Price: ${this.state.price}
+                                    </ControlLabel>
 
-                                <div>
-                                   <div2>
-                                       <ControlLabel
-                                           id = "sectionNumber"
-                                           style={{color: 'black', fontSize: 15}}>
-                                           Section Number:
-                                       </ControlLabel>
-                                   </div2>
-                                   <div2>
-                                       <FormControl style={{width: 50}} placeholder="Enter section #" type="section"
-                                           value={this.state.section}
-                                           onChange={this.handleChange.bind(this)}  />
-                                   </div2>
-                                </div>
+                                    <div>
+                                        <div2>
+                                            <ControlLabel
+                                                id="sectionNumber"
+                                                style={{color: 'black', fontSize: 15}}>
+                                                Section Number:
+                                            </ControlLabel>
+                                        </div2>
+                                        <div2>
+                                            <FormControl style={{width: 50}} placeholder="Enter section #"
+                                                         type="section"
+                                                         value={this.state.section}
+                                                         onChange={this.handleChange.bind(this)}/>
+                                        </div2>
+                                    </div>
 
-                                <div>
-                                   <Button bsStyle="primary"
-                                       onClick={this.getTicketsWithFilter.bind(this)}>
-                                       Apply
-                                   </Button>
-                                </div>
-                            </FormGroup>
-                        </Panel>
+                                    <div>
+                                        <Button bsStyle="primary"
+                                                onClick={this.getTicketsWithFilter.bind(this)}>
+                                            Apply
+                                        </Button>
+                                    </div>
+                                </FormGroup>
+                            </Panel>
 
                             <h3 className="Tickets-label"> Tickets </h3>
                             <TicketListItem ticketsFromPickTicket={this.state.tickets}/>
-                    </Col>
-                </Row>
-            </Grid>
+                        </Col>
+                    </Row>
+                </Grid>
+            </div>
+
         );
     }
 }
