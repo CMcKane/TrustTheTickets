e4081575df1d0d02da9cdbf24cc04407676b35b7
AnthonyOrio <orioa94@gmail.com>
2017-11-10 11:54:23 -0500

    Added zone selection

--- js/src/components/pick-tickets/pick-tickets.js
+++ js/src/components/pick-tickets/pick-tickets.js
@@ -12,6 +12,8 @@ import TicketListItem from './ticket-list-item';
 import queryString from 'query-string';
 import { ClimbingBoxLoader } from 'react-spinners';
 import {LinkContainer} from 'react-router-bootstrap';
+import Switch from 'react-toggle-switch';
+//import ToggleButton from 'react-toggle-button';
 
 var clickedSection = ''
 
@@ -33,7 +35,30 @@ export default class PickTickets extends Component {
             isLoading: false,
             previousSections: [],
             sliderValue: 0,
-            toggleValue: 1
+            toggleValue: 1,
+            chartToggleValue: 1,
+            bySection: true,
+            innerZone1: ["112", "113", "114", "101", "102", "124"],
+            innerZone2: ["103", "111", "115", "123"],
+            innerZone3: ["104", "105", "109", "110", "116", "117", "121", "122"],
+            innerZone4: ["106", "107", "108", "118", "119", "120"],
+            outerZone1: ["201", "202", "203", "211", "212", "213", "214", "215", "223", "224"],
+            outerZone2: ["204", "204A", "205", "205A", "209", "209A", "210", "210A", "216", "216A",
+                         "217", "217A", "222A", "222", "221A", "221"],
+            outerZone3: ["206", "207", "207A", "208", "220", "219A", "219", "218"],
+            noseBleeds: ["U1", "U2", "U3", "U4", "U5", "U6", "U7", "U8", "U9", "U10", "U11", "U12", "U13",
+                         "U14", "U15", "U16", "U17", "U18", "U19", "U20", "U21", "U22", "U23", "U24",
+                         "U25", "U26"],
+            suites: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15",
+                     "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29",
+                     "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43",
+                     "44", "45", "46", "47", "48", "49", "50", "51", "52", "53", "54", "55", "56", "57",
+                     "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "70", "71",
+                     "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82"],
+            clubBox1: ["CB2", "CB3", "CB4", "CB10", "CB11", "CB12", "CB14", "CB15", "CB16", "CB23", "CB24"],
+            clubBox2: ["CC21", "CC20","CC19","CC18","CC17"],
+            special: ["SB1", "SB13"],
+            private: ["PS22"]
         }
         this.getEvent();
         this.displayAllTickets();
@@ -175,31 +200,102 @@ export default class PickTickets extends Component {
     }
 
     onChartClick(section) {
-        if(this.state.sections.length === 1 && this.state.sections[0] === section) {
+        console.log(section);
+        if(this.state.bySection)
+        {
+            if(this.state.sections.length === 1 && this.state.sections[0] === section) {
+                this.setState({
+                    previousSections: this.state.sections,
+                    sections: [],
+                    tickets: [],
+                    toggleValue: 1
+                });
+            }
+            else {
+                this.setState({isLoading:true, tickets: []});
+                TTTPost('/tickets', {
+                    eventID: this.state.eventID,
+                    section_number: section
+                })
+                    .then(res => {
+                        if (res.data.tickets) {
+                            this.setState({
+                                previousSections: this.state.sections,
+                                sections: [section],
+                                tickets: res.data.tickets,
+                                isLoading: false,
+                                toggleValue: 1
+                            });
+                        }
+                    });
+            }
+        }
+        else
+        {
             this.setState({
+                isLoading:true, tickets: [],
                 previousSections: this.state.sections,
-                sections: [],
-                tickets: [],
-                toggleValue: 1
+                sections: this.determineSectionsZone(section)
             });
         }
-        else {
-            this.setState({isLoading:true, tickets: []});    
-            TTTPost('/tickets', {
-                eventID: this.state.eventID,
-                section_number: section
-            })
-                .then(res => {
-                    if (res.data.tickets) {
-                        this.setState({
-                            previousSections: this.state.sections,
-                            sections: [section],
-                            tickets: res.data.tickets,
-                            isLoading: false,
-                            toggleValue: 1
-                        });
-                    }
-                });
+
+    }
+
+    determineSectionsZone(section) {
+
+        var firstChar = section.charAt(0);
+        var len = section.length;
+
+        if(firstChar === '1' && len > 2)
+        {
+            for(var i = 0; i < this.state.innerZone1.length; i++)
+                if(section === this.state.innerZone1[i]) { return this.state.innerZone1; }
+            for(var i = 0; i < this.state.innerZone2.length; i++)
+                if(section === this.state.innerZone2[i]) { return this.state.innerZone2; }
+            for(var i = 0; i < this.state.innerZone3.length; i++)
+                if(section === this.state.innerZone3[i]) { return this.state.innerZone3; }
+            for(var i = 0; i < this.state.innerZone4.length; i++)
+                if(section === this.state.innerZone4[i]) { return this.state.innerZone4; }
+
+        }
+        else if(firstChar === '2' && len > 2)
+        {
+            for(var i = 0; i < this.state.outerZone1.length; i++)
+                if(section === this.state.outerZone1[i]) { return this.state.outerZone1; }
+            for(var i = 0; i < this.state.outerZone2.length; i++)
+                if(section === this.state.outerZone2[i]) { return this.state.outerZone2; }
+            for(var i = 0; i < this.state.outerZone3.length; i++)
+                if(section === this.state.outerZone3[i]) { return this.state.outerZone3; }
+        }
+        else if(firstChar === 'U')
+        {
+            return this.state.noseBleeds;
+        }
+        else if(len === 2)
+        {
+            return this.state.suites;
+        }
+        else if(firstChar === 'C')
+        {
+            if(section.charAt(1) === 'C') {
+                return this.state.clubBox2;
+            }
+            else {
+                return this.state.clubBox1;
+            }
+
+        }
+        else if(firstChar === 'S')
+        {
+            return this.state.special;
+        }
+        else if(firstChar === 'P')
+        {
+            return this.state.private;
+        }
+        else
+        {
+            return [];
         }
     }
 
@@ -225,6 +321,15 @@ export default class PickTickets extends Component {
 
     eventButtonClick() {
         return <LinkContainer to='/event-calendar'/>;
+    }
+
+    toggleSectionOrZone() {
+        if(this.state.bySection) {
+            this.setState({bySection: false, chartToggleValue: 2});
+        }
+        else {
+            this.setState({bySection: true, chartToggleValue: 1});
+        }
     }
 
     //render the values in the tickets
@@ -265,6 +370,17 @@ export default class PickTickets extends Component {
                                                 </LinkContainer>
                                             </DropdownButton>
                                         </ButtonGroup>
+                                        <div>
+                                            <ToggleButtonGroup
+                                                id = "chartToggleGroup"
+                                                name = "chartToggleGroup"
+                                                type="radio"
+                                                value={this.state.chartToggleValue}
+                                                onChange={this.toggleSectionOrZone.bind(this)}>
+                                                    <ToggleButton id="section" value={1} >Section</ToggleButton>
+                                                    <ToggleButton id="zone" value={2}>Zone</ToggleButton>
+                                            </ToggleButtonGroup>
+                                        </div>
                                     </Col>
                                 </Row>
                                 <br/>
@@ -307,11 +423,14 @@ export default class PickTickets extends Component {
                                         </ControlLabel>
 
                                         <div>
+
                                             <Button bsStyle="primary"
                                                     onClick={this.getTicketsWithFilter.bind(this)}>
                                                 Apply
                                             </Button>
+
                                         </div>
+
                                     </FormGroup>
                                 </Panel>
 
