8e179d5c08bac598fecc0ff33bebcf50712701c4
AnthonyOrio <orioa94@gmail.com>
2017-11-20 12:12:08 -0500

    Added redirecting for checkout.

--- js/src/components/checkout/checkout-view.js
+++ js/src/components/checkout/checkout-view.js
@@ -6,16 +6,27 @@ import {TTTPost, TTTGet} from '../backend/ttt-request';
 import { ClimbingBoxLoader } from 'react-spinners';
 import '../../stylesheet.css';
 import 'rc-slider/assets/index.css';
+import queryString from 'query-string';
+
 
 export default class CheckoutView extends Component {
 
     constructor(props) {
         super(props);
+
+        const queryParams = queryString.parse(this.props.location.search);
+        console.log(queryParams);
     }
     render() {
         return (
             <div className=" globalBody globalImage">
-                <div>  </div>
+                <Grid style={{paddingTop: "25px"}}>
+                    <h1>
+                        <Well className='checkoutHeader'>
+                            Checkout
+                        </Well>
+                    </h1>
+                </Grid>
             </div>
         );
     }
--- js/src/components/pick-tickets/pick-tickets-modal.js
+++ js/src/components/pick-tickets/pick-tickets-modal.js
@@ -2,6 +2,7 @@ import React, { Component }  from 'react';
 import { Button, Col, Modal, Form, FormGroup, ControlLabel, FormControl } from 'react-bootstrap';
 import {LinkContainer} from 'react-router-bootstrap';
 import Time from 'react-time';
+import { Redirect } from 'react-router-dom';
 import '../../stylesheet.css';
 
 
@@ -11,8 +12,21 @@ export default class PickTicketsModal extends Component {
     super(props);
 
     this.state = {
-      group: this.props.group
+      group: this.props.group,
+      redirect: false,
+      redirectLink: ''
     }
+  }
+
+  createCheckoutRedirectLink(e) {
+      var ticket = this.props.group[e.target.id];
+      this.setState({
+        redirect: true,
+        redirectLink: '/checkout-view?section=' + ticket.section_number +
+                                     '&row=' + ticket.row_number +
+                                     '&seat=' + ticket.seat_number +
+                                     '&price=' + ticket.ticket_price
+      });
   }
 
   getHeader() {
@@ -32,13 +46,15 @@ export default class PickTicketsModal extends Component {
                     <p className="ticketAttributes">Row: {currTicket.row_number} </p>
                     <p className="ticketAttributes">Seat: {currTicket.seat_number} </p>
                     <p className="ticketAttributes">Price: ${currTicket.ticket_price}
-                    <LinkContainer to="/checkout-view">
-                        <Button id={i} style={{marginLeft: "60px", color: "black"}} >Buy</Button>
-                    </LinkContainer>
+                    <Button
+                        id={i}
+                        style={{marginLeft: "60px", color: "black"}}
+                        onClick={this.createCheckoutRedirectLink.bind(this)}>
+                        Buy
+                    </Button>
                     </p>
                 </p>
             );
-
         }
         return(<div > {list} </div>);
     }
@@ -69,21 +85,28 @@ export default class PickTicketsModal extends Component {
   }
 
   render() {
-    return (
-      <Modal style={this.getCursorStatus()} onHide={this.onHide.bind(this)} 
-        show={this.props.show} bsSize="large" aria-labelledby="contained-modal-title-sm">
-        <Modal.Header className="pickTicketModalHeader" closeButton>
-          <Modal.Title id="contained-modal-title-sm">Tickets</Modal.Title>
-        </Modal.Header>
-        <Modal.Body className="pickTicketModalBody">
-          <h4>{this.getHeader()}</h4>
-          {this.getTicketContent()}
-        </Modal.Body>
-        <Modal.Footer className="pickTicketModalHeader">
-          {this.getErrorText()}
-          <Button onClick={() => this.props.onHide()}>Close</Button>
-        </Modal.Footer>
-      </Modal>
-    );
-  }
+        if(this.state.redirect)
+        {
+            return <Redirect to={this.state.redirectLink} />
+        }
+        else
+        {
+            return (
+              <Modal style={this.getCursorStatus()} onHide={this.onHide.bind(this)}
+                show={this.props.show} bsSize="large" aria-labelledby="contained-modal-title-sm">
+                <Modal.Header className="pickTicketModalHeader" closeButton>
+                  <Modal.Title id="contained-modal-title-sm">Tickets</Modal.Title>
+                </Modal.Header>
+                <Modal.Body className="pickTicketModalBody">
+                  <h4>{this.getHeader()}</h4>
+                  {this.getTicketContent()}
+                </Modal.Body>
+                <Modal.Footer className="pickTicketModalHeader">
+                  {this.getErrorText()}
+                  <Button onClick={() => this.props.onHide()}>Close</Button>
+                </Modal.Footer>
+              </Modal>
+            );
+        }
+    }
 }
