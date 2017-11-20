809847fa5bf4277e8cdadf82002af1d7fd8b79f0
AnthonyOrio <orioa94@gmail.com>
2017-11-20 13:55:14 -0500

    Created basic checkout

--- js/src/components/checkout/checkout-view.js
+++ js/src/components/checkout/checkout-view.js
@@ -1,22 +1,50 @@
 import React, {Component} from 'react';
 import {Grid, Row, Col, FormGroup, ControlLabel, FormControl, Well,
         Button, Panel, ToggleButtonGroup, ToggleButton, ButtonGroup,
-        DropdownButton, MenuItem, Modal} from 'react-bootstrap';
+        DropdownButton, MenuItem, Modal, Table} from 'react-bootstrap';
 import {TTTPost, TTTGet} from '../backend/ttt-request';
 import { ClimbingBoxLoader } from 'react-spinners';
 import '../../stylesheet.css';
 import 'rc-slider/assets/index.css';
 import queryString from 'query-string';
 
+var queryParams = null
 
 export default class CheckoutView extends Component {
 
     constructor(props) {
         super(props);
 
-        const queryParams = queryString.parse(this.props.location.search);
-        console.log(queryParams);
+        queryParams = queryString.parse(this.props.location.search);
+        console.log(queryParams.price);
+        this.state = {
+            group: this.props.group,
+            redirect: false,
+            redirectLink: ''
+        }
     }
+
+    buildTicketInfoRendering() {
+        return(
+            <table class="table">
+                <thead>
+                    <th className="tableHeading">Section</th>
+                    <th className="tableHeading">Row</th>
+                    <th className="tableHeading">Seat</th>
+                    <th className="tableHeading">Price</th>
+                </thead>
+                <tr>
+                    <td>{queryParams.section}</td>
+                    <td>{queryParams.row}</td>
+                    <td>{queryParams.seat}</td>
+                    <td>${queryParams.price}</td>
+                </tr>
+              </table>
+
+
+        );
+    }
+
     render() {
         return (
             <div className=" globalBody globalImage">
@@ -26,6 +54,7 @@ export default class CheckoutView extends Component {
                             Checkout
                         </Well>
                     </h1>
+                    {this.buildTicketInfoRendering()}
                 </Grid>
             </div>
         );
