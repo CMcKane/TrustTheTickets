0ca2fa1801fbf48df80e06c212b3358f36119b2b
Thomas Harker <harkert2@students.rowan.edu>
2017-11-22 17:47:13 -0500

    Created stored proc to reset ticket status id after 5 minutes after changing to inprogress.

--- js/src/components/pick-tickets/pick-tickets.js
+++ js/src/components/pick-tickets/pick-tickets.js
@@ -515,6 +515,7 @@ export default class PickTickets extends Component {
     }
 
     setCheckoutTickets(tickets) {
+        // Call backend to see if these tickets are all still available first
         this.setState({
             checkoutTickets: tickets,
             show: false,
@@ -563,11 +564,12 @@ export default class PickTickets extends Component {
     }
 
     render() {
-
+        console.log(this.state.eventID);
             if(this.state.checkoutPageActive)
             {
                 return (
-                    <Checkout checkoutTickets={this.state.checkoutTickets} />
+                    <Checkout returnRedirect={"/pick-tickets?event=" + this.state.eventID}
+                        checkoutTickets={this.state.checkoutTickets} />
                 );
             }
             else
