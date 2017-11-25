37636ef99b175625dae6a5f06996e781dcf25317
curtisbaillie1 <bailliec7@students.rowan.edu>
2017-11-25 01:25:00 -0500

    Revert "Minor bug fixes"
    This reverts commit 71df7b864c321414bfb66b4e50d2bc3423524a00.

--- py/account.py
+++ py/account.py
@@ -1,5 +1,9 @@
+#from passlib.hash import sha256_crypt
+
 class Account(object):
 
     def __init__(self, email, password):
         self.email = email
         self.password = password
+        # Hashes and salts password
+        # self.password = sha256_crypt.hash(password)
