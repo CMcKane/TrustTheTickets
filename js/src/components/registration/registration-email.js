04dd9b5c637840014995540af18b21a49f39d1b2
Thomas Harker <harkert2@students.rowan.edu>
2017-11-29 10:36:27 -0500

    Fixing mobile scrolling and registration fatal error.

--- js/src/components/registration/registration-email.js
+++ js/src/components/registration/registration-email.js
@@ -1,10 +1,11 @@
 import React, { Component }  from 'react';
+import '../../stylesheet.css';
 
 export default class RegistrationEmail extends Component {
 
 	render() {
 		return(
-			<div className="text-center" style={{color: 'white'}}>
+			<div className="text-center" style={{color: 'black'}}>
 				<p> You've been sent an email to confirm your email address. It should arrive shortly. </p>
 			</div>
 		);
