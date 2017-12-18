3443673a4ca877465e3bfd74a733a1c39a8e8022
AnthonyOrio <orioa94@gmail.com>
2017-12-17 22:02:05 -0500

    Documented account_login

--- py/account_login.py
+++ py/account_login.py
@@ -4,15 +4,25 @@ from account_jwt import JWTService
 
 jwt_service = JWTService()
 
+# This class is responsible with authenticating accounts.
 class AccountAuthenticator(object):
 
+    # The initialization method.
     def __init__(self, mysql):
         self.mysql = mysql
 
+    # Authenticates that email and password entered by a user are valid.
+    # param IN self - this class object.
+    # param IN data - the email and password entered.
+    # param OUT True if valid False if not.
     def authenticate_user(self, data):
         account = Account(data['email'], data['password'],'','','','','','','','')
         return self.verify_credentials(account)
 
+    # Verifies the user credentials.
+    # param IN self - this class object.
+    # param IN account - the account to verify credentials for.
+    # param OUT True if verfied False if not.
     def verify_credentials(self, account):
         sqlHandler = SqlHandler(self.mysql)
         loginResultDict = {'authenticated': False}
